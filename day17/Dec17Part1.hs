869561cff69942b0e79b65a09aed38978f13a927
Daniel Cohen <daniel.cohen@patientco.com>
2020-12-17 13:17:31 -0500

    Day17

---
+++ day17/Dec17Part1.hs
@@ -0,0 +1,56 @@
+import qualified Data.Map as Map
+
+type Vec3 = (Int, Int, Int)
+type Grid = Map.Map Vec3 Bool
+
+getGrid :: FilePath -> IO Grid
+getGrid path = do
+    contents <- readFile path
+    return $ Map.fromList $ concat [ map (\(x, char) -> case char of
+                                                            '.' -> ((x, y, 0), False)
+                                                            '#' -> ((x, y, 0), True)) (zip [0..] line) 
+                                    | (y, line) <- zip [0..] $ lines contents ]
+
+
+adjacent :: Vec3 -> [Vec3]
+adjacent (startX, startY, startZ) =
+    [(x, y, z) | 
+    x <- [startX-1..startX+1],
+    y <- [startY-1..startY+1],
+    z <- [startZ-1..startZ+1],
+    (x, y, z) /= (startX, startY, startZ)]
+
+
+applyRule :: Grid -> Vec3 -> (Vec3, Bool)
+applyRule grid pos =
+    if isActive && (elem activeNeighbors [2, 3]) then
+        (pos, True)
+    else if not isActive && (activeNeighbors == 3) then
+        (pos, True)
+    else
+        (pos, False)
+    where
+        neighbors = adjacent pos
+        isActive = case Map.lookup pos grid of
+                        Nothing -> False
+                        Just active -> active
+        activeNeighbors = length $ filter (\loc -> case Map.lookup loc grid of
+                                            Nothing -> False
+                                            Just active -> active) neighbors
+
+
+applyRules :: Grid -> Grid
+applyRules grid =
+    foldl (\newGrid (k, v) -> Map.insert k v newGrid) grid updateList
+    where
+        updateList = concat [ [ applyRule grid pos | pos <- adjacent cubePos ++ [cubePos] ] | cubePos <- Map.keys (Map.filter id grid) ]
+
+
+runRounds :: Grid -> Int -> Grid
+runRounds grid rounds =
+    foldl (\newGrid _ -> applyRules newGrid) grid [1..rounds]
+
+main = do
+    grid <- getGrid "input"
+    let finalGrid = runRounds grid 6
+    print $ Map.size (Map.filter id finalGrid)
